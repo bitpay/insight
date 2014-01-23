458875913a4a72f0ef4dc9fee58ec993cc34f650
Matias Alejo Garcia <ematiu@gmail.com>
2014-01-23 15:10:52 -0300

    insight working in livenet. WIP

--- public/js/controllers/status.js
+++ public/js/controllers/status.js
@@ -13,23 +13,21 @@ function($scope, $routeParams, $location, Global, Status, Sync, get_socket) {
       angular.extend($scope, d);
     },
     function(e) {
-      $scope.error = 'API ERROR: ' + e.data;
+      $scope.error =  'API ERROR: ' + e.data;
     });
   };
 
-  var on_sync_update = function(sync) {
-console.log('[status.js.21:sync:]',sync); //TODO
+  var onSyncUpdate = function(sync) {
     $scope.sync = sync;
   };
 
   $scope.getSync = function() {
     Sync.get({},
     function(sync) {
-      on_sync_update(sync);
+      onSyncUpdate(sync);
     },
     function(e) {
       var err = 'Could not get sync information' + e.toString();
-console.log('[status.js.30:err:]',err); //TODO
       $scope.sync = { error: err };
     });
   };
@@ -37,7 +35,7 @@ console.log('[status.js.30:err:]',err); //TODO
   var socket = get_socket($scope);
   socket.emit('subscribe', 'sync');
   socket.on('status', function(sync) {
-    on_sync_update(sync);
+    onSyncUpdate(sync);
   });
 
 });
