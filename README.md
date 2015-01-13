0f0fed6eab6bc91a4b7b4f8e9b66d5736f44f319
Esotericizm <esotericizm@cryptoservices.net>
2015-01-13 15:34:09 +1000

    DigiExplorer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # *insight*
 
-*insight* is an open-source bitcoin blockchain explorer with complete REST
+*insight* is an open-source digibyte blockchain explorer with complete REST
 and websocket APIs. Insight runs in NodeJS, uses AngularJS for the
 front-end and LevelDB for storage.
 
@@ -33,7 +33,7 @@ Check some screenshots and more details at [insight's project homepage](http://i
 
     http://localhost:3001
 
-  If *insight* reports problems connecting to **bitcoind** please check the CONFIGURATION section of 
+  If *insight* reports problems connecting to **digibyted** please check the CONFIGURATION section of 
   [insight-api README](https://github.com/bitpay/insight-api/blob/master/README.md). To set the 
   environment variables run something like:
   
--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: '/views/block.html',
-      title: 'Bitcoin Block '
+      title: 'Digibyte Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: '/views/transaction.html',
-      title: 'Bitcoin Transaction '
+      title: 'Digibyte Transaction '
     }).
     when('/', {
       templateUrl: '/views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: '/views/block_list.html',
-      title: 'Bitcoin Blocks solved Today'
+      title: 'Digibyte Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: '/views/block_list.html',
-      title: 'Bitcoin Blocks solved '
+      title: 'Digibyte Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: '/views/address.html',
-      title: 'Bitcoin Address '
+      title: 'Digibyte Address '
     }).
     when('/status', {
       templateUrl: '/views/status.html',
