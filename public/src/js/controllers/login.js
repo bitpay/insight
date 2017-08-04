f0fb72400e505b6ce883e96fbcd162bc8f06362d
wangxp2016 <wangxp_2014@163.com>
2017-08-04 01:13:12 -0700

    edit login

--- public/src/js/controllers/login.js
+++ public/src/js/controllers/login.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('insight.login').controller('loginCtrl',
-    function($scope,$localStorage,Account,$state) {
+angular.module('insight.login').controller('loginController',function($scope,$localStorage,Account,$state) {
 //依赖注入的内容 作用域 本地 账户信息 弹出提示 状态值
     $scope.login = function () {
         if ($scope.user && $scope.user.name && $scope.user.password) { //如果用户和用户名及密码都是正确的
@@ -10,7 +9,7 @@ angular.module('insight.login').controller('loginCtrl',
                     $localStorage.username = data.name;
                     $localStorage.telephone = data.phone;
                     $scope.$emit('login', data.userId);
-                    $state.go('/index-template');    //登录成功跳转页面
+                    $state.go('home');    //登录成功跳转页面
                 },function(data){
                     if (data.code !== 200) {
                        // SweetAlert.swal("", "用户名或密码错误", "warning");
