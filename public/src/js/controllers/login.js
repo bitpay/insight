935a425fdee2a152a11e417c76711cfd40415c17
caozhongzheng <caozhongzheng@peersafe.cn>
2017-08-09 09:58:54 +0800

    Add: login page logic

--- public/src/js/controllers/login.js
+++ public/src/js/controllers/login.js
@@ -2,26 +2,19 @@
 
 angular.module('insight.login').controller('loginController',
     function($scope, $rootScope, $routeParams, $location,Account) {
-    $rootScope.isLogin=false;
+    $scope.isLogin = function () {
+        console.log('insight.logi loginController isLogin:', $rootScope.isLogin);
+        return $rootScope.isLogin;
+    }
 //依赖注入的内容 作用域 本地 账户信息 弹出提示 状态值
     $scope.login = function () {
       
        if ($scope.user && $scope.user.name && $scope.user.password) { //如果用户和用户名及密码都是正确的
-         $location.path('/home');  
-         $rootScope.isLogin=true;
-          /*  Account.signIn($scope.user, function (data) {   //account.signIn是登录方法 登录后本地获取用户信息
-                  console.log(data);
-                    $localStorage.userId = data.userId;
-                    $localStorage.username = data.name;
-                    $localStorage.telephone = data.phone;
-                    $location.path('/home');    //登录成功跳转页面
-                },function(data){
-                    if (data.code !== 200) {
-                       // SweetAlert.swal("", "用户名或密码错误", "warning");
-                        console.log(data);
-                    }
-                })
-*/
+           $rootScope.isLogin = Account.login($scope.user);
+           console.log('login user info:', $scope.user.name,  $scope.user.password);
+           if ($rootScope.isLogin) {
+               $location.path('/home');
+           }
         }
         else {
             //SweetAlert.swal("", "请重新输入用户名或密码", "warning");
--- public/src/js/services/login.js
+++ public/src/js/services/login.js
@@ -1,26 +1,35 @@
 'use strict';
 
-angular.module('insight.login').factory('Account', function () {
-    var authService = {
-        userId:1,
-        name:"admin",
-        password:123456,
-        phone:13645784152,
-        message:"test messafe"
-    };
+angular.module('insight.login')
+    .factory('Account', function () {
+        var users = [{
+            userId: 1,
+            name: "admin",
+            password: "123456",
+            phone: 13645784152,
+            message: "test messafe"
+        }];
 
-   /* signIn = function (user) {
-        if(user.name=="admin"&&user.password==123456){
-            return authService;
-        }else{
+        var service = {};
 
-        }
-        return $http
-            .post('/login', credentials)
-            .then(function (res) {
-                Session.create(res.data.id, res.data.user.id,
-                    res.data.user.role);
-                return res.data.user;
-            });
-    };*/
-});
+        service.login = function (user) {
+            //if this user already in the list, ignore it.
+            for (i in users) {
+                // console.log(typeof users[i].name,  typeof user.name);
+                if (users[i].name == user.name && users[i].password == user.password) {
+                    console.log('login user info OKOKOK:', users[i],user);
+                    return true;
+                }
+            }
+
+            // console.log(typeof users.name,  typeof user.name);
+            // if (users[i].name == user.name && users[i].password == user.password) {
+            //     console.log('login user info OKOKOK:', users[i],user);
+            //     return true;
+            // }
+            return false;
+        };
+
+        return service;
+
+    });
