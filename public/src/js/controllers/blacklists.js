db7e28248b19218dceff12f31a7e7310d56d400e
caozhongzheng <caozhongzheng@peersafe.cn>
2017-08-12 11:58:21 +0800

    Add:黑名单用需求要求的UI方式实现

--- public/src/js/controllers/blacklists.js
+++ public/src/js/controllers/blacklists.js
@@ -20,38 +20,125 @@ angular.module('insight.blacklists').controller('BlacklistsController',
 
     $scope.bedit = true;
 
-    $scope.saveBlacklist = function () {
+    // open add dialog----------------------------------------
+    $scope.addMode = function () {
+      $scope.isAdd = true;
+    };
+
+    // click dialog's add btn
+    $scope.doAdd = function () {
+      if (!$scope.newblacklist || !$scope.newblacklist.addr) {
+        // TODO 还需要验证地址的有效性
+        $scope.newblacklist = {addr:'please input address'}
+      } else if ($scope.newblacklist.addr.length !== 34) {
+        // TODO 还需要验证地址的有效性
+        $scope.newblacklist.addr = 'invalid address'
+      } else {
+        $scope.isAdd = false;
+        $scope.saveBlacklist($scope.newblacklist);
+      }
+    };
+
+    // click dialog's cancel btn
+    $scope.cancelAdd = function () {
+      $scope.newblacklist.addr = ''
+      $scope.newblacklist.comment = ''
+      $scope.isAdd = false;
+    };
+
+    // click edit btn----------------------------------------
+    $scope.editMode = function () {
+      $scope.bedit = false;
+    };
+
+    // open edit dialog
+    $scope.openEdit = function (addr, comment) {
+      console.log('edit ',addr,'with cmt:',comment);
+      $scope.editblacklist = {
+        addr: addr,
+        comment: comment
+      };
+      $scope.isEdit = true;
+      console.log('edit ',$scope.editblacklist,'with switch:',$scope.isEdit);
+    }
+
+    // click dialog edit btn
+    $scope.doEdit = function () {
+      $scope.saveBlacklist($scope.editblacklist);
+      $scope.isEdit = false;
+    }
+
+    // click dialog's cancel btn
+    $scope.cancelEdit = function () {
+      $scope.editblacklist.addr = ''
+      $scope.editblacklist.comment = ''
+      $scope.isEdit = false;
+    };
+
+    // click cancel btn----------------------------------------
+    $scope.cancelMode = function () {
+      $scope.bedit = true;
+    };
+
+    $scope.checked = [];
+    $scope.selectAll = function () {
+      if ($scope.select_all) {
+        $scope.checked = [];
+        angular.forEach($scope.blacklists, function (i) {
+          i.checked = true;
+          $scope.checked.push(i.addr);
+        })
+      } else {
+        angular.forEach($scope.blacklists, function (i) {
+          i.checked = false;
+          $scope.checked = [];
+        })
+      }
+      console.log($scope.checked);
+    };
+    $scope.selectOne = function () {
+      angular.forEach($scope.blacklists, function (i) {
+        var index = $scope.checked.indexOf(i.addr);
+        if (i.checked && index === -1) {
+          $scope.checked.push(i.addr);
+        } else if (!i.checked && index !== -1) {
+          $scope.checked.splice(index, 1);
+        }
+        ;
+      })
+
+      if ($scope.blacklists.length === $scope.checked.length) {
+        $scope.select_all = true;
+      } else {
+        $scope.select_all = false;
+      }
+      console.log($scope.checked);
+    }
+
+    $scope.saveBlacklist = function (blacklist) {
       // BlacklistService.save($scope.newblacklist);
 
       // console.log('$scope.newblacklist.addr=',$scope.newblacklist.addr)
       // console.log('$scope.newblacklist.comment=',$scope.newblacklist.comment)
 
       // console.log($scope.bedit,'SAVE: ',$scope.newblacklist)
-      if ($scope.newblacklist === {}
-        || $scope.newblacklist === undefined
-        || $scope.newblacklist.addr === undefined
-        || $scope.newblacklist.addr.length !== 34
-      ) {
-        $scope.bedit = true;
-        return;
-      }
 
       // $scope.status = 'loading';
       if (!$scope.bedit) {
-        $http.patch(Service.apiPrefix + '/blacklist/' + $scope.newblacklist.addr,
-          $scope.newblacklist)
+        $http.patch(Service.apiPrefix + '/blacklist/' + blacklist.addr,
+          blacklist)
           .success(function(data, status, headers, config) {
             if (data.code === 0) {
               $scope.list();
             }
-            $scope.newblacklist = {}
+            $scope.editblacklist = {}
           })
           .error(function(data, status, headers, config) {
           });
 
         // console.log("after edit list=", $scope.blacklists);
       } else {
-        $http.post(Service.apiPrefix + '/blacklist', $scope.newblacklist)
+        $http.post(Service.apiPrefix + '/blacklist', blacklist)
           .success(function(data, status, headers, config) {
             if (data.code === 0) {
               $scope.list();
@@ -102,6 +189,7 @@ angular.module('insight.blacklists').controller('BlacklistsController',
       return str.join("&");
     }
 
+    // form eitd.(old)
     $scope.edit = function (hash, bz) {
       $scope.newblacklist = {
         addr: hash,
