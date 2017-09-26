7155ff0b9129afed75b3183cea766f3667e273c9
Chris Kleeschulte <chrisk@bitpay.com>
2017-09-26 17:13:22 -0400

    Added a build runner in line with starting the service.

--- bitcore-node/index.js
+++ bitcore-node/index.js
@@ -3,15 +3,13 @@
 var BaseService = require('./service');
 var inherits = require('util').inherits;
 var fs = require('fs');
-var pkg = require('../package');
+var exec = require('child_process').exec;
+var pkg = require('../package.json');
 
 var InsightUI = function(options) {
   BaseService.call(this, options);
-  // we don't use the options object for routePrefix and apiPrefix, since the
-  // client must be rebuilt with the proper options. A future version of 
-  // Bitcore should allow for a service "build" step to make this better.
-  this.apiPrefix = pkg.insightConfig.apiPrefix;
-  this.routePrefix = pkg.insightConfig.routePrefix;
+  this.apiPrefix = options.apiPrefix || 'api';
+  this.routePrefix = options.routePrefix || '';
 };
 
 InsightUI.dependencies = ['insight-api'];
@@ -19,8 +17,21 @@ InsightUI.dependencies = ['insight-api'];
 inherits(InsightUI, BaseService);
 
 InsightUI.prototype.start = function(callback) {
-  this.indexFile = this.filterIndexHTML(fs.readFileSync(__dirname + '/../public/index-template.html', {encoding: 'utf8'}));
-  setImmediate(callback);
+
+  var self = this;
+  pkg.insightConfig.apiPrefix = self.apiPrefix;
+  pkg.insightConfig.routePrefix = self.routePrefix;
+
+  fs.writeFileSync(__dirname + '/../package.json', JSON.stringify(pkg, null, 2));
+  exec('pushd ' + __dirname + '/../;' +
+    ' npm run install-and-build; popd', function(err) {
+    if (err) {
+      return callback(err);
+    }
+    self.indexFile = self.filterIndexHTML(fs.readFileSync(__dirname + '/../public/index-template.html', {encoding: 'utf8'}));
+    callback();
+  });
+
 };
 
 InsightUI.prototype.getRoutePrefix = function() {
@@ -31,7 +42,7 @@ InsightUI.prototype.setupRoutes = function(app, express) {
   var self = this;
   app.use(express.static(__dirname + '/../public'));
   // if not in found, fall back to indexFile (404 is handled client-side)
-  app.use(function(req, res, next) {
+  app.use(function(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.send(self.indexFile);
   });
