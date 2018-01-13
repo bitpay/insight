9cd4ce910dad112c37ffbba06690cf5710fb91ef
Vincent Swarte <vincent@vinsert.nl>
2018-01-13 20:36:26 +0100

    replaced all instances of BTC with VIA

--- public/src/js/controllers/header.js
+++ public/src/js/controllers/header.js
@@ -7,7 +7,7 @@ angular.module('insight.system').controller('HeaderController',
     $rootScope.currency = {
       factor: 1,
       bitstamp: 0,
-      symbol: 'BTC'
+      symbol: 'VIA'
     };
 
     $scope.menu = [{
