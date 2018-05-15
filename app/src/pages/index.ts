290ebd4f6dabb5c389e928c61adbb3db9997d33a
Darren Nelsen <darren@bitpay.com>
2018-05-15 17:01:23 -0400

    fix address layout; use proper push technique with nav controller

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -2,5 +2,7 @@ export * from './nodeStatusPage/nodeStatusPage';
 export * from './verifyMessagePage/verifyMessagePage';
 export * from './home/home';
 export * from './blocks/blocks';
+export * from './address/address';
+export * from './transaction/transaction';
 export * from './broadcast-tx/broadcast-tx';
 export * from './pages.module';
--- app/src/pages/pages.module.ts
+++ app/src/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { NgModule }         from '@angular/core';
 import { IonicModule }      from 'ionic-angular';
 import { ComponentsModule } from '../components';
 import { BlocksPageModule } from '../pages/blocks/blocks.module';
+import { AddressPageModule } from '../pages/address/address.module';
+import { TransactionPageModule } from '../pages/transaction/transaction.module';
 import { BroadcastTxPageModule } from './broadcast-tx/broadcast-tx.module';
 import { HeadNavComponentModule } from '../components/head-nav/head-nav.module';
 import { LatestTransactionsComponentModule } from '../components/latest-transactions/latest-transactions.module';
@@ -22,13 +24,14 @@ import {
     IonicModule,
     ComponentsModule,
     BlocksPageModule,
+    AddressPageModule,
+    TransactionPageModule,
     BroadcastTxPageModule,
     HeadNavComponentModule,
     LatestTransactionsComponentModule,
     LatestBlocksComponentModule
   ],
   exports: [
-    // CustomComponent,
   ],
   entryComponents: [],
   providers: []
